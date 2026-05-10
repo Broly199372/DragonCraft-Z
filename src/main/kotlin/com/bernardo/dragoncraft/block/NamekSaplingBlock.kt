e988f923513f24c9dcf452daf9c68ef6c86ce7ff
Broly199372 <bernardopf2011@gmail.com>
2026-05-10 19:29:45 +0000

    bug

--- src/main/kotlin/com/bernardo/dragoncraft/block/NamekSaplingBlock.kt
+++ src/main/kotlin/com/bernardo/dragoncraft/block/NamekSaplingBlock.kt
@@ -3,4 +3,7 @@ package com.bernardo.dragoncraft.block
 import net.minecraft.block.SaplingBlock
 import net.minecraft.block.sapling.OakSaplingGenerator
 
+
 class NamekSaplingBlock(settings: Settings) : SaplingBlock(OakSaplingGenerator(), settings)
+
+
