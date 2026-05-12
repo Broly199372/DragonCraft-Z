cb154036384a1c4f93264159e121c767b2e8d2a8
Broly199372 <bernardopf2011@gmail.com>
2026-05-12 19:31:15 +0000

    Fix: Clean merge conflict markers from Kotlin and JSON files

--- src/main/kotlin/com/bernardo/dragoncraft/block/NamekSaplingBlock.kt
+++ src/main/kotlin/com/bernardo/dragoncraft/block/NamekSaplingBlock.kt
@@ -1,11 +1,8 @@
 package com.bernardo.dragoncraft.block
+
 import net.minecraft.block.SaplingBlock
 import net.minecraft.block.sapling.OakSaplingGenerator
-<<<<<<< HEAD
-=======
 
-
->>>>>>> e988f923513f24c9dcf452daf9c68ef6c86ce7ff
 class NamekSaplingBlock(settings: Settings) : SaplingBlock(OakSaplingGenerator(), settings)
 
 
